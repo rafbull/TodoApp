062bab86cd0c1146918c3dabe4e6e5a2a382e806
Rafis Bulatov <rafbullstuff@gmail.com>
2024-06-21 19:13:18 +0300

    Create FileCacheError

---
+++ ToDoApp/Errors/FileCacheError.swift
@@ -0,0 +1,21 @@
+//
+//  FileCacheError.swift
+//  TestRepoToDo_Yandex
+//
+//  Created by Rafis on 19.06.2024.
+//
+
+import Foundation
+
+enum FileCacheError: Error {
+case badID(_ id: String)
+}
+
+extension FileCacheError {
+    var errorDescription: String {
+        switch self {
+        case .badID(let id):
+            return "⚠️ This TodoItem ID is already in use! ID: \"\(id)\""
+        }
+    }
+}
