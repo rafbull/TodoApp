ac02933a82e881bb074ba01c287ed25de581c4e6
Rafis Bulatov <rafbullstuff@gmail.com>
2024-06-29 05:54:49 +0300

    Add AppColor and AppFont

---
+++ ToDoApp/Resources/AppColor.swift
@@ -0,0 +1,31 @@
+//
+//  AppColor.swift
+//  ToDoApp
+//
+//  Created by Rafis on 26.06.2024.
+//
+
+import SwiftUI
+
+enum AppColor {
+    static let navBarBlurSupport = Color("navBarBlurSupport")
+    static let overlaySupport = Color("overlaySupport")
+    static let separatorSupport = Color("separatorSupport")
+    
+    static let disableLabel = Color("disableLabel")
+    static let primaryLabel = Color("primaryLabel")
+    static let secondaryLabel = Color("secondaryLabel")
+    static let tertiaryLabel = Color("tertiaryLabel")
+    
+    static let blue = Color("blue")
+    static let gray = Color("gray")
+    static let grayLight = Color("grayLight")
+    static let green = Color("green")
+    static let red = Color("red")
+    static let white = Color("white")
+    
+    static let elevatedBackground = Color("elevatedBackground")
+    static let primaryBackground = Color("primaryBackground")
+    static let secondaryBackground = Color("secondaryBackground")
+    static let systemBackgound = Color("systemBackgound")
+}
---
+++ ToDoApp/Resources/AppFont.swift
@@ -0,0 +1,17 @@
+//
+//  AppFont.swift
+//  ToDoApp
+//
+//  Created by Rafis on 26.06.2024.
+//
+
+import SwiftUI
+
+enum AppFont {
+    static let largeTitle = Font.system(size: 38, weight: .bold, design: .default)
+    static let title = Font.system(size: 20, weight: .semibold, design: .default)
+    static let headline = Font.system(size: 17, weight: .semibold, design: .default)
+    static let body = Font.system(size: 17, weight: .regular, design: .default)
+    static let subhead = Font.system(size: 15, weight: .regular, design: .default)
+    static let footnote = Font.system(size: 13, weight: .semibold, design: .default)
+}
